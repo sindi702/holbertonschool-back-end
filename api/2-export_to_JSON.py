6616ce52c58cac70af30b1fc7ea2dc4d4a22bfda
sindi702 <hamatajsindi@gmail.com>
2023-08-22 11:28:52 +0200

    test

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -15,13 +15,15 @@ if __name__ == "__main__":
     user_data = user_api.text
     user = json.loads(user_data)
     todos = json.loads(todo_data)
-    all_todos = []
+    completed = []
+    all_todos = 0
     for todo in todos:
         if todo['userId'] == user['id']:
-            all_todos.append(
-                {"task": todo['title'], "completed": todo['completed'],
-                "username": user['username']})
-    user_tasks = {str(user['id']): all_todos}
-    filename = "{}.json".format(user['id'])
-    with open(filename, 'w') as file:
-        json.dump(user_tasks, file)
+            if todo['completed']:
+                completed.append(todo)
+            all_todos += 1
+    print(
+        'Employee {} is done with tasks({}/{}):'
+        .format(user['name'], len(completed), all_todos), file=sys.stdout)
+    for finished_todo in completed:
+        print('\t {}'.format(finished_todo['title']), file=sys.stdout)
